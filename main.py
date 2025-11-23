90e45f6256f60127df014f29c27353a69a4b038a
Zinchuk Alex <zinchukalex@gmail.com>
2025-11-23 21:12:17 +0200

    Resolve merge conflicts

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
+
 def get_string(string: str, number: int) -> str:
     return string * number
 
 
 print(get_string.__annotations__)
-=======
+
 print('hello')
->>>>>>> 4cfb12cce9c5168a6b7ab324d7d8a86d8e315952
+
+
